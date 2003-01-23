131590d4318873e62aed8c6142e0026b81be3c91
htl10 <htl10>
2003-01-23 15:43:17 +0000

    Initial revision

---
+++ epl_print_stripe.c
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2003 Hin-Tak Leung, Roberto Ragusa
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+**/
+
+#include <stdio.h>
+#include "epl_job.h"
+
+int epl_print_stripe(EPL_job_info *epl_job_info, typ_stream *stream) 
+{
+  char temp_string[256];
+  char *ts;
+  int count;
+  int e;
+
+#ifdef EPL_DEBUG
+  fprintf(stderr, "EPL print stripe\n");
+#endif
+
+  /* this part does the model-dependent stripe header */
+
+  ts = temp_string;
+  count = stream->count;
+  if(epl_job_info->model == MODEL_5700L) 
+    {
+      ts += sprintf(ts, "%c%c%c%c%c%c%c",
+        0x04,
+	0x00,
+	0x01,
+        count >> 24, count >> 16, count >> 8, count
+	);
+    }
+  
+  else if(epl_job_info->model == MODEL_5800L
+          || epl_job_info->model == MODEL_5900L) 
+    {
+      ts += sprintf(ts, "\x01d%deps{I%c%c%c%c%c%c%c",
+	count + 7,
+	0x06,
+	0x00,
+	0x01,
+	count >> 24, count >> 16, count >> 8, count
+	);
+    }
+  
+#ifdef EPL_DEBUG
+  fprintf(stderr, "Writing %s stripe header (%i bytes)\n",
+          printername[epl_job_info->model], ts - temp_string);
+#endif
+  e = fwrite(temp_string, 1, ts - temp_string, epl_job_info->outfile);
+  if(e != ts - temp_string) return -1;
+
+  /* model-independent part - dump the stream body */
+
+  e = fwrite(stream->start, 1, count, epl_job_info->outfile);
+  if(e != count) return -1;
+
+  return 0;
+} 
